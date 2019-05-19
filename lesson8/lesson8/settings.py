8deefa9ddabb7b3bfb95994fabc2dbbd3bac708e
Billy Huang <10646003@ntub.edu.tw>
2019-05-20 05:15:30 +0800

    lesson8 - Books finished

--- lesson8/lesson8/settings.py
+++ lesson8/lesson8/settings.py
@@ -64,9 +64,9 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'lesson8.pipelines.Lesson8Pipeline': 300,
-#}
+ITEM_PIPELINES = {
+   'lesson8.pipelines.BookPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/autothrottle.html
@@ -88,3 +88,8 @@ ROBOTSTXT_OBEY = True
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+# Configure Exporter
+FEED_URI = 'export_data/%(name)s.csv'
+FEED_FORMAT = 'csv'
+FEED_EXPORT_FIELDS = ['upc', 'name', 'price', 'stock', 'review_rating', 'review_num']
