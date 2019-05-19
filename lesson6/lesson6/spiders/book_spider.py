dd2525466d744881ae4e633a5a599fab20562165
Billy Huang <10646003@ntub.edu.tw>
2019-05-20 02:39:06 +0800

    lesson6 - Link Extractor finished

--- lesson6/lesson6/spiders/book_spider.py
+++ lesson6/lesson6/spiders/book_spider.py
@@ -1,4 +1,5 @@
 import scrapy
+from scrapy.linkextractors import LinkExtractor
 
 from ..items import BookItem
 
@@ -16,9 +17,9 @@ class BooksSpider(scrapy.Spider):
 
             yield book
 
-        next_url = response.css('ul.pager li.next a::attr(href)') \
-                           .extract_first()
+        le = LinkExtractor(restrict_css='ul.pager li.next')
+        links = le.extract_links(response)
 
-        if next_url:
-            next_url = response.urljoin(next_url)
+        if links:
+            next_url = links[0].url
             yield scrapy.Request(next_url, callback=self.parse)
