c9da2f63eb6e3c5a124d02706a84af0d8e1f15d9
Billy Huang <10646003@ntub.edu.tw>
2019-07-01 12:22:05 +0800

    lesson12-4 - redis finished

--- lesson12_4/lesson12_4/pipelines.py
+++ lesson12_4/lesson12_4/pipelines.py
@@ -5,6 +5,10 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+from scrapy import Item
+
+import redis
+
 
 class BookPipeline(object):
     review_rating_map = {
@@ -21,3 +25,31 @@ class BookPipeline(object):
             item['review_rating'] = self.review_rating_map[rating]
 
         return item
+
+
+class RedisPipeline(object):
+    def open_spider(self, spider):
+        db_host = spider.settings.get('REDIS_HOST', 'localhost')
+        db_port = spider.settings.get('REDIS_PORT', 6379)
+        db_index = spider.settings.get('REDIS_DB_INDEX', 0)
+
+        # 連接資料庫
+        self.db_connect = redis.StrictRedis(host=db_host, port=db_port, db=db_index)
+        self.item_index = 0
+
+    def close_spider(self, spider):
+        # 關閉連接
+        self.db_connect.connection_pool.disconnect()
+
+    def process_item(self, item, spider):
+        self.insert_db(item)
+
+        return item
+
+    def insert_db(self, item):
+        if isinstance(item, Item):
+            item = dict(item)
+
+        self.item_index += 1
+        # 將資料以 Hash 類型(雜湊) 儲存到 Redis 中
+        self.db_connect.hmset('book:{}'.format(self.item_index), item)
--- lesson12_4/lesson12_4/settings.py
+++ lesson12_4/lesson12_4/settings.py
@@ -66,6 +66,7 @@ ROBOTSTXT_OBEY = True
 # See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
     'lesson12_4.pipelines.BookPipeline': 300,
+    'lesson12_4.pipelines.RedisPipeline': 400,
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
@@ -88,3 +89,8 @@ ITEM_PIPELINES = {
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+# Configure redis
+REDIS_HOST = 'localhost'
+REDIS_PORT = 6379
+REDIS_DB_INDEX = 0
