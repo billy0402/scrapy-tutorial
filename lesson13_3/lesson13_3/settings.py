faf88c852e66027a6d28f9412eca5f72cbfc7b1b
Billy Huang <10646003@ntub.edu.tw>
2019-07-03 21:22:44 +0800

    lesson13-3 - random Proxy finished

--- lesson13_3/lesson13_3/settings.py
+++ lesson13_3/lesson13_3/settings.py
@@ -9,6 +9,8 @@
 #     https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://doc.scrapy.org/en/latest/topics/spider-middleware.html
 
+from fake_useragent import UserAgent
+
 BOT_NAME = 'lesson13_3'
 
 SPIDER_MODULES = ['lesson13_3.spiders']
@@ -16,10 +18,12 @@ NEWSPIDER_MODULE = 'lesson13_3.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'lesson13_3 (+http://www.yourdomain.com)'
+# 偽裝成正常瀏覽器
+USER_AGENT = UserAgent().random
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+# 我們的爬取不符合豆瓣爬取的規則，強制爬取
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
@@ -27,7 +31,8 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+# (可選)設定下載延遲，防止代理被封禁 IP，依據代理數量而定
+DOWNLOAD_DELAY = 5
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -52,9 +57,9 @@ ROBOTSTXT_OBEY = True
 
 # Enable or disable downloader middlewares
 # See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    'lesson13_3.middlewares.Lesson133DownloaderMiddleware': 543,
-#}
+DOWNLOADER_MIDDLEWARES = {
+    'lesson13_3.middlewares.RandomHttpProxyMiddleware': 745,
+}
 
 # Enable or disable extensions
 # See https://doc.scrapy.org/en/latest/topics/extensions.html
@@ -88,3 +93,10 @@ ROBOTSTXT_OBEY = True
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+# Configure Proxy List
+HTTPPROXY_PROXY_LIST_FILE = '../lesson13_1/export_data/free_proxy_list.json'
+
+# Configure Exporter
+FEED_URI = 'export_data/%(name)s.json'
+FEED_FORMAT = 'json'
--- lesson13_3/lesson13_3/spiders/movies.py
+++ lesson13_3/lesson13_3/spiders/movies.py
@@ -1,11 +1,62 @@
 # -*- coding: utf-8 -*-
+import json
+import re
+from pprint import pprint
+
 import scrapy
+from scrapy import Request
 
 
 class MoviesSpider(scrapy.Spider):
+    BASE_URL = 'https://movie.douban.com/j/search_subjects?type=movie&tag={}&sort=recommend&page_limit={}&page_start={}'
+    MOVIE_TAG = '豆瓣高分'
+    PAGE_LIMIT = 20
+    page_start = 0
+
     name = 'movies'
     allowed_domains = ['movie.douban.com']
-    start_urls = ['http://movie.douban.com/']
+    start_urls = [BASE_URL.format(MOVIE_TAG, PAGE_LIMIT, page_start)]
 
     def parse(self, response):
-        pass
+        # 使用 json 模組解析回應結果
+        infos = json.loads(response.body.decode('utf8'))
+        movie_infos = infos['subjects']
+
+        # 反覆運算影片資訊列表
+        for movie_info in movie_infos:
+            movie_item = dict()
+
+            # 分析 片名 和 評分，填入 item
+            movie_item['片名'] = movie_info['title']
+            movie_item['評分'] = movie_info['rate']
+
+            # 分析影片頁面 url，建置 Request 發送請求，並將 item 透過 meta 參數傳遞給影片頁面解析函數
+            yield Request(movie_info['url'], callback=self.parse_movie,
+                          meta={'_movie_item': movie_item})
+
+        # 如果 json 結果中包含的影片數量小於請求數量，說明沒有影片了，否則繼續搜索
+        if len(movie_infos) == self.PAGE_LIMIT:
+            self.page_start += self.PAGE_LIMIT
+            url = self.BASE_URL.format(self.MOVIE_TAG, self.PAGE_LIMIT, self.page_start)
+
+            yield Request(url)
+
+    def parse_movie(self, response):
+        # 從 meta 中分析已包含 片名 和 評分 資訊的 item
+        movie_item = response.meta['_movie_item']
+
+        # 取得整個資訊字串
+        info = response.css('div.subject div#info').xpath('string(.)').extract_first()
+
+        # 分析所有欄位名稱
+        fields = [s.strip().replace(':', '') \
+                  for s in response.css('div#info span.pl::text').extract()]
+
+        # 分析所有欄位的值
+        values = [re.sub('\s+', ' ', s.strip()) \
+                  for s in re.split('\s*(?:%s):\s*' % '|'.join(fields), info)][1:]
+
+        # 將所有資訊填入 item
+        movie_item.update(dict(zip(fields, values)))
+
+        yield movie_item
